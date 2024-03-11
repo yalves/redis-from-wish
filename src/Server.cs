999a4f7d053b9ee524cf9825a6b3d104d78baf6c
Yan Alves <yanalves@hotmail.com>
2024-03-11 19:25:58 -0300

    Add support for multiple clients

--- src/Server.cs
+++ src/Server.cs
@@ -8,15 +8,22 @@ TcpListener server = new(IPAddress.Any, 6379);
 try
 {
     server.Start();
-    using var socket = await server.AcceptSocketAsync();
-    var buffer = new byte[1024];
+    
 
     while (true)
     {
-        var bytes = await socket.ReceiveAsync(buffer, SocketFlags.None);
-        Console.WriteLine(Encoding.UTF8.GetString(buffer[0..bytes]));
+        var socket = server.AcceptSocket();
+        Task.Run(() => HandleRequest(socket));
 
-        await socket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
+        async Task HandleRequest(Socket socket)
+        {
+            var buffer = new byte[1024];
+            while (true)
+            {
+                _ = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), SocketFlags.None);
+                await socket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
+            }            
+        }        
     }
 }
 finally 
