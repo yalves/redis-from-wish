bf3504f46c368a6484ccef20d58a498a66dc9208
Yan Alves <yanalves@hotmail.com>
2024-03-11 19:02:21 -0300

    Add PONG response to PING

--- src/Server.cs
+++ src/Server.cs
@@ -1,10 +1,26 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
-TcpListener server = new TcpListener(IPAddress.Any, 6379);
-server.Start();
-server.AcceptSocket(); // wait for client
+TcpListener server = new(IPAddress.Any, 6379);
+try
+{
+    server.Start();
+    using var socket = await server.AcceptSocketAsync();
+
+    var buffer = new byte[1024];
+    var bytes = await socket.ReceiveAsync(buffer, SocketFlags.None);
+    Console.WriteLine(Encoding.UTF8.GetString(buffer[0..bytes]));
+
+    await socket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
+}
+finally
+{
+    server.Stop();
+}
+
+
