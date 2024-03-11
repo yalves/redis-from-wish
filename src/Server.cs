8a984d87440828567709eed47e311a871b54cfde
Yan Alves <yanalves@hotmail.com>
2024-03-11 19:08:01 -0300

    Respond to multiple pings

--- src/Server.cs
+++ src/Server.cs
@@ -2,25 +2,27 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
 TcpListener server = new(IPAddress.Any, 6379);
 try
 {
     server.Start();
     using var socket = await server.AcceptSocketAsync();
-
     var buffer = new byte[1024];
-    var bytes = await socket.ReceiveAsync(buffer, SocketFlags.None);
-    Console.WriteLine(Encoding.UTF8.GetString(buffer[0..bytes]));
 
-    await socket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
+    while (true)
+    {
+        var bytes = await socket.ReceiveAsync(buffer, SocketFlags.None);
+        Console.WriteLine(Encoding.UTF8.GetString(buffer[0..bytes]));
+
+        await socket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
+    }
 }
-finally
-{
-    server.Stop();
+finally 
+{ 
+    server.Stop(); 
 }
+
 
 
